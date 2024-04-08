830056de5b01c0729543b5eb67d4aa240e27804b
Doneux <85197474+Doneux@users.noreply.github.com>
2024-04-08 00:08:06 -0700

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,8 +1,14 @@
+#first we get the text from the user and store it in variable ins
 ins=input("Insert text: ")
-ins2=ins.replace("a","v")
-ins3=ins2.replace("e","v")
-ins4=ins3.replace("i","v")
-ins5=ins4.replace("o","v")
-ins6=ins5.replace("u","v")
-print(ins6)
+#then, we send variable ins through a very brief series of 5 vowel changes.
+#note, this cipher will not change capital letters. this is by design, as it allows some leeway to an experienced user.
+ins=ins.replace("a","v") #replace all uses of a
+ins=ins.replace("e","v") #replace all uses of e
+ins=ins.replace("i","v") #replace all uses of i
+ins=ins.replace("o","v") #replace all uses of o
+ins=ins.replace("u","v") #replace all uses of u
+#to the recipient, assuming they are versed in this cipher, the text is readable upon delivery. no decoding is needed, nor is it possible.
+#now, we print the result. change to return for a function if necessary.
+print(ins)
+#the program will now quit, as it has completed it's purpose.
 quit()
